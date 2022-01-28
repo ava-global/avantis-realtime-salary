d8282d4b91e0dd91ff0c719d48e295369b36fb06
dodadoa <maddodadoa@gmail.com>
2022-01-28 16:20:10 +0700

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Wallet } from './Wallet'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Real Time Salary
+      </header>
+      <Wallet />
+    </div>
+  );
+}
+
+export default App;
